5cca26c178ebb21ccc565c7a08be3e2d845fda2c
Megha-Dev-19 <meghadev1901@gmail.com>
2023-09-11 20:11:54 +0530

    updated cast votes component

--- NDC/Elections/CastVotes.js
+++ NDC/Elections/CastVotes.js
@@ -124,7 +124,7 @@ const CastVotes = () => (
         ) : blacklisted ? (
           <span>Your account is blacklisted</span>
         ) : (
-          <span>Make sure you selected {seats} candidates</span>
+          <span>Make sure you selected {seats} candidates. You can only vote once per house</span>
         )}
       </Info>
     </div>
@@ -145,11 +145,13 @@ const CastVotes = () => (
         src={widgets.styledComponents}
         props={{
           Button: {
-            className: "primary justify-content-center",
+            className: "justify-content-center " +  seats - myVotesForHouse().length - selectedCandidates.length > 0 ? " bg-secondary text-white" : " primary",
             disabled: selectedCandidates.length === 0 || blacklisted,
-            text: `Cast ${selectedCandidates.length || ""} Vote${
-              selectedCandidates.length === 1 ? "" : "s"
-            }`,
+            text: `Cast ${alreadyVotedForHouse()
+              ? 0
+              : seats - myVotesForHouse().length - selectedCandidates.length} / ${seats} Vote${
+                selectedCandidates.length === 1 ? "" : "s"
+              } `,
             onClick: handleCast,
           },
         }}
